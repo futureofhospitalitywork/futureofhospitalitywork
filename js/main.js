cdb0c7e556b712380ed202e8b63d5a2b926bd0af
Future of Hospitality Work <oterry@andrew.cmu.edu>
2024-06-12 09:11:16 +0000

    comments

--- js/main.js
+++ js/main.js
@@ -4,22 +4,28 @@ var scroll = window.requestAnimationFrame
   || window.mozRequestAnimationFrame
   || window.msRequestAnimationFrame
   || window.oRequestAnimationFrame
-  || function(callback){ window.setTimeout(callback, 1000/60) };
+  || function (callback) { window.setTimeout(callback, 1000 / 60) };
+
+// variable to store the last scroll position
 var lastPosition = -1;
 
+// variables for parallax transition
 var lastSection = false;
 var replaceItemTop = -1;
 var replaceItemBottom = -1;
 var replaceItemHeight = -1;
- 
-// scroll function
-function loop(){
+
+// scroll function for parallax logo animation (switching from light to dark section and vice versa)
+function loop() {
+  // current scroll position
   var top = window.pageYOffset;
 
+  // selecting sections and elements for replacement
   var sections = document.querySelectorAll('.section');
   var replaceContainer = document.querySelectorAll('.js-replace');
   var replaceItem = document.querySelectorAll('.js-replace__item');
 
+  // calculate dimensions of replace item
   if (replaceItem.length > 0) {
     // get top position of item from container, because image might not have loaded
     replaceItemTop = parseInt(replaceContainer[0].getBoundingClientRect().top);
@@ -27,76 +33,84 @@ function loop(){
     replaceItemBottom = replaceItemTop + replaceItemHeight;
   }
 
+  // Variables to store section dimensions and state
   var sectionTop = -1;
   var sectionBottom = -1;
   var currentSection = -1;
-  
-  // Fire when needed
+
+  // Fire when needed (check if scrolling)
   if (lastPosition == window.pageYOffset) {
     scroll(loop);
     return false;
   } else {
     lastPosition = window.pageYOffset;
 
-  Array.prototype.forEach.call(sections, function(el, i){
-    sectionTop = parseInt(el.getBoundingClientRect().top);
-    sectionBottom = parseInt(el.getBoundingClientRect().bottom);
+    // Loop through sections
+    Array.prototype.forEach.call(sections, function (el, i) {
+      sectionTop = parseInt(el.getBoundingClientRect().top);
+      sectionBottom = parseInt(el.getBoundingClientRect().bottom);
 
+      // Determine if current section is light or dark
+      if ((sectionTop <= replaceItemBottom) && (sectionBottom > replaceItemTop)) {
+        currentSection = el.classList.contains('section--bg');
 
-    if ( (sectionTop <= replaceItemBottom) && (sectionBottom > replaceItemTop)) {
-      currentSection = el.classList.contains('section--bg');
-
-      if ( currentSection ) { 
-        replaceContainer[0].classList.remove('js-replace--reverse');
-      } else {
-        replaceContainer[0].classList.add('js-replace--reverse')
+        // toggle class for replacing container based on current section
+        if (currentSection) {
+          replaceContainer[0].classList.remove('js-replace--reverse');
+        } else {
+          replaceContainer[0].classList.add('js-replace--reverse')
+        }
       }
-    }
 
-    if ( (replaceItemTop < sectionTop) && ( sectionTop <= replaceItemBottom) ) {
-      if (currentSection != lastSection)  {
-        document.documentElement.style.setProperty('--replace-offset', 100 / replaceItemHeight * parseInt(sectionTop - replaceItemTop) + '%');
+      // adjust parallax effect based on scroll position
+      if ((replaceItemTop < sectionTop) && (sectionTop <= replaceItemBottom)) {
+        if (currentSection != lastSection) {
+          document.documentElement.style.setProperty('--replace-offset', 100 / replaceItemHeight * parseInt(sectionTop - replaceItemTop) + '%');
+        }
       }
-    }
 
-    if ( replaceItemTop >= sectionTop ) {
-      document.documentElement.style.setProperty('--replace-offset', 0 + '%');
-      lastSection = currentSection;
-    }
+      // reset parallax effect if section is above the replace item
+      if (replaceItemTop >= sectionTop) {
+        document.documentElement.style.setProperty('--replace-offset', 0 + '%');
+        lastSection = currentSection;
+      }
 
-  }); 
+    });
 
+  }
+  // continue animation loop
+  scroll(loop)
 }
-
-scroll( loop )
-}
-
+// initialize animation loop
 loop();
 
-window.onresize = function(event) {
+// re-run loop on window resize
+window.onresize = function (event) {
   loop();
 };
 
+/*Toggle button text for file viewer*/
 function showDIS() {
-    var x = document.getElementById("dis-paper");
-    var y = document.getElementById("dis-button");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.innerHTML = "Hide Paper";
-    } else {
-      x.style.display = "none";
-      y.innerHTML = "View Paper";
-    }
+  var x = document.getElementById("dis-paper");
+  var y = document.getElementById("dis-button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML = "Hide Paper";
+  } else {
+    x.style.display = "none";
+    y.innerHTML = "View Paper";
   }
+}
 
-  function showCSCW() {
-    var x = document.getElementById("cscw-paper");
-    var y = document.getElementById("cscw-button");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.innerHTML = "Hide Paper";
-    } else {
-      x.style.display = "none";
-      y.innerHTML = "View Paper";
-    }
+/*Toggle button text for file viewer*/
+function showCSCW() {
+  var x = document.getElementById("cscw-paper");
+  var y = document.getElementById("cscw-button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML = "Hide Paper";
+  } else {
+    x.style.display = "none";
+    y.innerHTML = "View Paper";
   }
+}

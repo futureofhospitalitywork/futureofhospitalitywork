2c9ec3840d00cc9719f7451965e71f89abf65cb0
Future of Hospitality Work <oterry@andrew.cmu.edu>
2024-07-03 17:09:20 +0000

    hamburger menu for mobile

--- js/main.js
+++ js/main.js
@@ -114,3 +114,18 @@ function showCSCW() {
     y.innerHTML = "View Paper";
   }
 }
+
+// script.js
+$(document).ready(function() {
+  $('#mobile-menu').click(function() {
+      // Toggle display property of #nav-bar
+      if ($('.nav-bar').css('display') === 'none') {
+          $('.nav-bar').css('display', 'flex');
+      } else {
+          $('.nav-bar').css('display', 'none');
+      }
+
+      // Toggle active class on menu icon
+      $(this).toggleClass('active');
+  });
+});
